eea27c724a0f297b706da47bce2c7856526196c7
bhavya <bhavyabommagani@my.unt.edu>
2023-10-21 00:06:48 +0530

    updated code with 3 more apis

--- src/routes/protected-routes.js
+++ src/routes/protected-routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const verifyAccessToken = require("../middlewares/verifyAccessToken");
 const cosmosClient = require("../db");
+const { learningVideosContent } = require("../utils");
 
 const router = express.Router();
 
@@ -51,6 +52,86 @@ router.post("/post-learning-videos", verifyAccessToken, async (req, res) => {
     res.status(201).json({ message: "Links inserted successfully", results });
   } catch (error) {
     res.status(500).json({ message: "Failed to insert links", error });
+  }
+});
+
+router.get("/get-learning-content", verifyAccessToken, (req, res) => {
+  res.status(200).json(learningVideosContent);
+});
+
+router.post("/set-learning-progress", verifyAccessToken, async (req, res) => {
+  try {
+    const { contentTitle } = req.body;
+
+    if (!contentTitle) {
+      return res.status(400).json({ message: "Content title is required" });
+    }
+
+    const { resources: userDetails } = await cosmosClient
+      .database(process.env.COSMOS_DB)
+      .container(process.env.COSMOS_CONTAINER_USER_DETAILS)
+      .items.query({
+        query: "SELECT * FROM c WHERE c.email = @email",
+        parameters: [
+          {
+            name: "@email",
+            value: req.user.email,
+          },
+        ],
+      })
+      .fetchAll();
+
+    if (userDetails.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    userDetails[0].lastTopic = contentTitle;
+
+    const { statusCode } = await cosmosClient
+      .database(process.env.COSMOS_DB)
+      .container(process.env.COSMOS_CONTAINER_USER_DETAILS)
+      .item(userDetails[0].id)
+      .replace(userDetails[0]);
+
+    if (statusCode !== 200) {
+      return res.status(500).json({ message: "Failed to update user details" });
+    }
+
+    res.status(200).json({ message: "Learning progress updated successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to update learning progress", error });
+  }
+});
+
+router.get("/get-learning-progress", verifyAccessToken, async (req, res) => {
+  try {
+    const { resources: userDetails } = await cosmosClient
+      .database(process.env.COSMOS_DB)
+      .container(process.env.COSMOS_CONTAINER_USER_DETAILS)
+      .items.query({
+        query: "SELECT * FROM c WHERE c.email = @email",
+        parameters: [
+          {
+            name: "@email",
+            value: req.user.email,
+          },
+        ],
+      })
+      .fetchAll();
+
+    if (userDetails.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const lastTopic = userDetails[0].lastTopic;
+
+    res.status(200).json({ lastTopic });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to retrieve learning progress", error });
   }
 });
 
---
+++ src/utils.js
@@ -0,0 +1,15 @@
+const learningVideosContent = {
+  topics: ["topic 1", "topic 2"],
+  content: [
+    {
+      title: "topic 1",
+      description: "Topic 1 description",
+    },
+    {
+      title: "topic 2",
+      description: "Topic 2 description",
+    },
+  ],
+};
+
+module.exports = { learningVideosContent };
