a26951b8f825c8765815887be306686da89d3f08
Manish Manda <manishmanda@my.unt.edu>
2023-10-22 23:38:20 -0500

    added content

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,37 @@
 const learningVideosContent = {
-  topics: ["topic 1", "topic 2"],
+  topics: ["What Is Web Accessibility?", "Accessibility is Important for Individuals, Businesses, Society","The Four Principles of Accessibility"],
   content: [
     {
-      title: "topic 1",
-      description: "Topic 1 description",
+      title: "What Is Web Accessibility?",
+      description: `We’ve all experienced it: the annoyance at a slow-loading website, the squinting at badly-designed font, or the despair of trying to navigate a mobile unfriendly page. While these problems may be a slight inconvenience to us, for people with disabilities, they can completely restrict their internet use. Web accessibility is the need for websites to utilize tools and technologies developed to aid the perception, understanding, contribution, navigation, and interaction of a person with disabilities on the site. Integrating accessibility can seem intimidating to those that are just getting acquainted with it, but it is a vital element of user experience. Accessibility should be built into the web development and design process, rather than trying to retrofit it as an afterthought.
+      Accessible sites present information through multiple sensory channels, such as sound and sight, and they allow for additional means of site navigation and interactivity beyond the typical point-and-click-interface: keyboard-based control and voice-based navigation. The combination of a multisensory approach and a multi-interactivity approach allows disabled users to access the same information as nondisabled users.
+
+`,
     },
     {
-      title: "topic 2",
-      description: "Topic 2 description",
+      title: "Accessibility is Important for Individuals, Businesses, Society",
+      description: `The Web is an increasingly important resource in many aspects of life: education, employment, government, commerce, health care, recreation, and more. It is essential that the Web be accessible in order to provide equal access and equal opportunity to people with diverse abilities. Access to information and communications technologies, including the Web, is defined as a basic human right in the United Nations Convention on the Rights of Persons with Disabilities (UN CRPD).
+
+      The Web offers the possibility of unprecedented access to information and interaction for many people with disabilities. That is, the accessibility barriers to print, audio, and visual media can be much more easily overcome through web technologies.
+      
+      Accessibility supports social inclusion for people with disabilities as well as others, such as:
+      
+      older people
+      people in rural areas
+      people in developing countries
+      There is also a strong business case for accessibility. As shown in the previous section, accessible design improves overall user experience and satisfaction, especially in a variety of situations, across different devices, and for older users. Accessibility can enhance your brand, drive innovation, and extend your market reach.
+      
+      Web accessibility is required by law in many situations.`
     },
+    {
+      title:`The Four Principles of Accessibility`,
+      description:`The principles of web accessibility are the foundations of content produced for the web and for anyone who wants to use the web. These principles are known as POUR, which is an acronym that describes functional accessibility:
+      Perceivable - Perceivability refers to the information and elements of user interface that must be presented in a manner that can be perceived by the senses and that nothing is left undetectable or invisible. To most web users, perceivability is based primarily on visuals, but for those that are unable to, sound and touch are used instead.\n
+      Operable - Interactive interface elements such as controls, buttons, navigation and more should be operable. This means that a user must be able to operate interface elements by first identifying them, and for most by physically clicking, tapping, swiping, or rolling. For those that can’t interact in these ways, voice commands or the use of other assistive devices like head wands and eye trackers.
+      Understandable - This means that technology should be clear and consistent in the presentation and format, with predictable patterns of usage and design. End users should have no issue in comprehending the meaning and purpose of the information presented in the content while discerning the user flow and operation of the interface.
+      Robust - Robustness is the ability for content to function reliably by a wide variety of technologies, including assistive devices.
+      The lack of any one of these four principles will thus make the web inaccessible to users with disabilities.`
+    }
   ],
 };
 
